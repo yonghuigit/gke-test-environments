f718fc3e2c202266811418f1af5e12353ee4dfc5
Yonghui Zhang <yzhang@itradenetwork.com>
2020-06-25 22:50:48 -0700

    enable persistence for grafana

--- monitoring/setup.sh
+++ monitoring/setup.sh
@@ -5,3 +5,5 @@ helm uninstall grafana
 echo "Installing"
 helm install prometheus stable/prometheus -f prometheus_values.yaml
 helm install grafana stable/grafana -f grafana_values.yaml
+kubectl apply -f gke-managed-certs.yaml
+kubectl apply -f gke-monitor-ingress.yaml
